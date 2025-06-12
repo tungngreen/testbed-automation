7b49eb0454284debaa6a4266e64318a4d8856f66
tungngreen <29793179+tungngreen@users.noreply.github.com>
2025-06-12 17:32:06 +0900

    - Add overall playbook - Add host minty as a desktop - Minor name changes

--- ansible/server-install.sh
+++ ansible/server-install.sh
@@ -5,21 +5,14 @@ hostname=${1}
 read -p "Please enter the vault password: " vault_password
 
 ### SSH setup
-/bin/bash ssh/populate_known_hosts.sh
-echo ${vault_password} | ansible-playbook -i inventory/hosts ssh/populate_server_ssh_keys.yml --ask-vault-pass --limit ${hostname}
+/bin/bash hosts/populate_known_hosts.sh inventory/hosts
 
-### Setup fish
-echo ${vault_password} | ansible-playbook -i inventory/hosts fish/fish-setup.yml --ask-vault-pass --limit ${hostname}
+### If host name is all, run the playbook for all hosts without "--limit"
+if hostname == "all"; then
+  limit=""
+else
+  limit="--limit ${hostname}"
+fi
 
-### Docker setup
-echo ${vault_password} | ansible-playbook -i inventory/hosts docker-setup/setup.yml --ask-vault-pass --limit ${hostname}
-
-### NVIDIA CUDA, CUDNN setup
-echo ${vault_password} | ansible-playbook -i inventory/hosts nvidia/cuda_server.yml --ask-vault-pass --limit ${hostname}
-echo ${vault_password} | ansible-playbook -i inventory/hosts nvidia/cudnn_server.yml --ask-vault-pass --limit ${hostname}
-### NVIDIA Docker setup
-echo ${vault_password} | ansible-playbook -i inventory/hosts nvidia/nvidia-toolkit_server.yml --ask-vault-pass --limit ${hostname}
-echo ${vault_password} | ansible-playbook -i inventory/hosts nvidia/configure_docker_daemon.yml --ask-vault-pass --limit ${hostname}
-
-
+ansible-playbook -i inventory/hosts server-install.yml --ask-vault-pass ${limit}
 

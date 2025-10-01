84d710213ffef635d2e32484c8fdbe4feb865c30
tungngreen <29793179+tungngreen@users.noreply.github.com>
2025-10-01 14:41:34 +0900

    Add jetson overall script

---
+++ ansible/jetson-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+hostname=${1}
+
+read -p "Please enter the vault password: " vault_password
+
+### SSH setup
+/bin/bash hosts/populate_known_hosts.sh inventory/hosts
+
+### If host name is all, run the playbook for all hosts without "--limit"
+if hostname == "all"; then
+  limit=""
+else
+  limit="--limit ${hostname}"
+fi
+
+ansible-playbook -i inventory/hosts jetson-install.yml --ask-vault-pass ${limit}
+
